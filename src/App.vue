f89bf6a6503a1989e7c5089c64c229569a65ed43
jhonreyj6 <j6cafe2018@gmail.com>
2024-07-27 10:24:43 +0800

    save

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import Child from "./components/Child.vue";
+
+const parentMethod = () => {
+  alert('parent is called');
+}
+
+const childRef = ref(null);
+
+const callChildMethod = () => {
+  if (childRef.value) {
+    childRef.value.childMethod();
+  }
+}; 
+
+const myData = ref({
+    name: "yer",
+    age: 30,
+});
+
+const updatedData = ref();
+
+watch(updatedData, () => {
+  myData.value = updatedData.value;
+})
+
+</script>
+
+<template>
+  <div>
+    <button type="button" @click="callChildMethod" style="margin-bottom: 14px">Call Child Method</button>
+    <child @parentMethod="parentMethod" :data="myData" v-model:isUpdatedData="updatedData" ref="childRef">Children</child>
+  </div> 
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Child.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineEmits, watch } from "vue"
+
+const emit = defineEmits(['parentMethod', 'update:isUpdatedData'])
+
+const childMethod = () => {
+  alert('child is called');
+}
+
+defineExpose({
+  childMethod,
+});
+
+const triggerParentMethod = () => {
+  emit('parentMethod');
+}
+
+const props = defineProps({
+  data: {
+    type: Object,
+  }
+});
+
+const modifyData = () => {
+    emit("update:isUpdatedData", {name: "Jhon Rey", Age: 31});
+}
+
+</script>
+<template>
+  <div>
+    <p><button type="button" @click="triggerParentMethod">Call Parent Method</button></p>
+    
+    <p><button type="button" @click="modifyData">Update New Data</button></p>
+    
+    <p><slot /></p>
+    <p>{{props}}</p>
+  </div>
+</template>
